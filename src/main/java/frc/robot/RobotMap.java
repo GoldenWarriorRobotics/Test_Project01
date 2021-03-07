c25bf0641e85667d5ae547acffc41aaf03726fe0
alchemist5253 <lohitthrish@gmail.com>
2021-03-06 18:04:55 -0600

    test comment

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 /**
+ *
  * The RobotMap is a mapping from the ports sensors and actuators are wired into to a variable name.
  * This provides flexibility changing wiring, makes checking the wiring easier and significantly
  * reduces the number of magic numbers floating around.
@@ -33,4 +34,5 @@ public class RobotMap {
   // number and the module. For example you with a rangefinder:
   // public static int rangefinderPort = 1;
   // public static int rangefinderModule = 1;
+	// test comment
 }
