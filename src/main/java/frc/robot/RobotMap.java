1085d4ab259e74fe95c3dc6ed0581e9636c82938
minijithendra <57692237+minijithendra@users.noreply.github.com>
2021-03-11 16:17:51 -0600

    joystickTeleop

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into to a variable name.
+ * This provides flexibility changing wiring, makes checking the wiring easier and significantly
+ * reduces the number of magic numbers floating around.
+ */
+public class RobotMap {
+  
+public static int leftMaster = 1;
+  public static int leftFollower = 2;
+  public static int rightMaster = 3;
+  public static int rightFollower= 4;
+  public static int joystickPort = 0;
+
+  // For example to map the left and right motors, you could define the
+  // following variables to use with your drivetrain subsystem.
+  // public static int leftMotor = 1;
+  // public static int rightMotor = 2;
+
+  // If you are using multiple modules, make sure to define both the port
+  // number and the module. For example you with a rangefinder:
+  // public static int rangefinderPort = 1;
+  // public static int rangefinderModule = 1;
+}
---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.RobotMap;
+import frc.robot.commands.manualDrive;
+
+/** Add your docs here. */
+public class DriveTrain extends Subsystem {
+  private static CANSparkMaxLowLevel motorLeft1 = new CANSparkMax(RobotMap.leftMaster,MotorType.kBrushed);
+  private static CANSparkMaxLowLevel motorLeft2 = new CANSparkMax(RobotMap.leftFollower,MotorType.kBrushed);
+  private static CANSparkMaxLowLevel motorRight1 = new CANSparkMax(RobotMap.rightMaster, MotorType.kBrushed);
+  private static CANSparkMaxLowLevel motorRight2 = new CANSparkMax(RobotMap.rightFollower,MotorType.kBrushed);
+
+  SpeedControllerGroup leftMotorGroup = new SpeedControllerGroup(motorLeft1, motorLeft2);
+  SpeedController rightMotorGroup = new SpeedControllerGroup(motorRight1, motorRight2);
+
+  public DifferentialDrive drive = new DifferentialDrive(leftMotorGroup, rightMotorGroup);
+
+  public void manualDrive( double move, double turn) {
+    drive.arcadeDrive(move, turn);
+
+  
+  }
+  public void moveDriveTrain(double leftSpeed, double rightSpeed){
+    drive.tankDrive(leftSpeed, rightSpeed);
+
+
+  }
+ 
+
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+     setDefaultCommand(new manualDrive());
+  }
+}
